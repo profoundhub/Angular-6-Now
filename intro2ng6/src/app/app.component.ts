0560ba926fb0e93a30b506d6960f0d459059ff3f
Daniel Lim <hbt@markhamboards.com>
2018-04-12 22:18:41 -0400

    clean

--- intro2ng6/src/app/app.component.ts
+++ intro2ng6/src/app/app.component.ts
@@ -1,15 +1,11 @@
 import { Component } from '@angular/core';
 
-function log(className) {
-  console.log(className);
-}
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-@log
+
 export class AppComponent {
   title = 'NG 6 Apps';
  
