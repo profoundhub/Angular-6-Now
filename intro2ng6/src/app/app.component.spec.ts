893b3a7e8c944e79c18aca3978365a8ce2ab8eeb
Daniel Lim <hbt@markhamboards.com>
2018-04-12 21:23:16 -0400

    + updates

--- intro2ng6/src/app/app.component.spec.ts
+++ intro2ng6/src/app/app.component.spec.ts
@@ -8,16 +8,19 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   }));
+
   it('should create the NG 6 App', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
+
   it(`should have as title 'NG 6 App'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('NG 6 App');
   }));
+
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
