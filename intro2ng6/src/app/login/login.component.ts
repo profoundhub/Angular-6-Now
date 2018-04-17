b94d0205945f96f87c4d3ac88643e77de8b4fff8
Daniel Lim <hbt@markhamboards.com>
2018-04-16 22:55:37 -0400

    + Router

--- intro2ng6/src/app/login/login.component.ts
+++ intro2ng6/src/app/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
 
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
