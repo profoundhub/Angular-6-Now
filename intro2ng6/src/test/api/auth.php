d2b338c8f2e32eaabd0ea5602d0295bfed1fc75d
Daniel Lim <hbt@markhamboards.com>
2018-04-16 22:43:14 -0400

    what now

--- intro2ng6/src/test/api/auth.php
+++ intro2ng6/src/test/api/auth.php
@@ -1,5 +1,9 @@
 <?php 
 
+session_start();
+
+$_POST = json_decode(file_get_contents('php://input'), true);
+
 if (isset($_POST) && !empty($_POST)) {
     $username = $_POST['username'];
     $password = $_POST['password'];
