7a1a28bc867c145e6186d873a62de887d03385f3
Daniel Lim <hbt@markhamboards.com>
2018-04-16 22:04:40 -0400

    api         "target": {

--- intro2ng6/src/app/app.module.ts
+++ intro2ng6/src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
 import { AuthService } from './auth.service';
 import { LoginComponent } from './login/login.component';
 import { AdminComponent } from './admin/admin.component';
