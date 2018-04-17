9c30350cacb213b847b43e3e07d2503717b6ebab
Daniel Lim <hbt@markhamboards.com>
2018-04-16 22:08:07 -0400

    update build

--- intro2ng6/src/app/app.module.ts
+++ intro2ng6/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { AuthService } from './auth.service';
 import { LoginComponent } from './login/login.component';
